5837bd69367933ad720fb680e882de4641c5ed35
Justin Doubikin <justin.doubikin@uon.edu.au>
2019-04-02 21:57:10 +0000

    dunno what i changed

--- Project1/Project1.c
+++ Project1/Project1.c
@@ -1,49 +1,53 @@
-/**
- * need message 
- * message is in ascii
- * change to 0 and 26 
- * 
- * 
- * decrypt it 
- * 
- * 
- */
-
 #include<stdio.h>
 
 int main() {
+// 1. Encryption of a message with a rotation cipher given the message text and rotation amount
     char inputText[100];
     int i = 0;
     char x;
     int key; 
     printf("enter text to be encrypted:\n");
-    scanf("%s", inputText);
+    fgets(inputText, 100, stdin);
+    //scanf("%s", inputText);
     
     printf("enter key:\n");
     scanf("%d", &key);
 
-    for (i = 0; inputText[i] != '\0'; ++i) {
-      x = inputText[i];
-      
-      if(x >= 'a' && x <= 'z') {
-          x = x + key;
-          
-          if(x > 'z')
-            x = x - 'z' + 'a' - 1;
-            
-      }else if(x >= 'A' && x <= 'Z') {
-          x = x + key;
-          
-          if (x > 'Z')
-            x = x -'Z' + 'A' - 1;
-      }
-     inputText[i] = x;  
     
-    }
+    
+        for (i = 0; inputText[i] != '\0'; ++i) {
+          x = inputText[i];
+
+          if(x >= 'a' && x <= 'z') {
+              x = x + key;
+
+              if(x > 'z')
+                x = x - 'z' + 'a' - 1;
+
+          }else if(x >= 'A' && x <= 'Z') {
+              x = x + key;
+
+              if (x > 'Z')
+                x = x -'Z' + 'A' - 1;
+          }
+         inputText[i] = x;
+
+     }
+
     printf("Encrypted message: %s\n", inputText);
     
     return 0;
+// 2. Decryption of a message encrypted with a rotation cipher given cipher text and rotation amount
+// 3. Encryption of a message with a substitution cipher given message text and alphabet substitution
+// 4. Decryption of a message encrypted with a substitution cipher given cipher text and substitutions
+// 5. Decryption of a message encrypted with a rotation cipher given cipher text only
+// 6. Decryption of a message encrypted with a substitution cipher given cipher text only
 }
+
+
+
+
+
 
 /**
     
@@ -58,13 +62,15 @@ int main() {
 			//call decryption function decryption(c)
 			return;
 }
-
+//this would only work if the letters a to z were re assigned values of 0 - 25//
+||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 int encryption(int x) {
 	return (message + key)(% 26);
 }
 int decryption(int c) {
 	return (cipher - key)(% 26);
 }
+||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 */
 
 
