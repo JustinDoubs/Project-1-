6802ad754eb68bfbe88166368208ea09c624b0d0
Justin Doubikin <justin.doubikin@uon.edu.au>
2019-04-02 01:00:16 +0000

    moved project1.c to a Project1 folder

---
+++ Project1/Project1.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int encyption(int x);
+
+
+int main() {
+  printf("hello world");
+}
+
+int encryption(int x) {
+    return (message + key)(% 26);
+}
+
+
+
+/** 
+ *  to do- 
+ * -----------------------------------------
+ * create a key which assigns all letters to numbers from 0 to 25
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
+
+
+
+
+
+subsititution cipher is array stuff stuff stuff. 
+
