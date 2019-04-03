1b3789669e700e52bf74321bbcc75174c0c274b1
Justin Doubikin <justin.doubikin@uon.edu.au>
2019-04-03 09:10:48 +0000

    rrrrrrr

--- Project1/Project1.c
+++ Project1/Project1.c
@@ -15,6 +15,8 @@ int main() {
         "4 for 'Decryption of a message encrypted with a substitution cipher given cipher text and substitutions'\n"
         "5 for 'Decryption of a message encrypted with a rotation cipher given cipher text only'\n"
         "6 for 'Decryption of a message encrypted with a substitution cipher given cipher text only'\n");
+        
+        scanf("%d",&choice);
     
         switch (choice) {
             case '1':
@@ -32,6 +34,16 @@ int main() {
                 // 1. Encryption of a message with a rotation cipher given the message text and rotation amount
                 break;
             case '2':
+                char inputText[100]; //  array with ample range for any text input
+                int key; // key* determines how far the message is translated 
+                printf("enter text to be encrypted:\n"); // takes user input for text to be encrypted
+                fgets(inputText, 100, stdin); // stores user user input in array 'inputText'  have used fgets in lieu of anything bettter as i dont know how to stroe white space in an array... 
+                
+    
+                printf("enter key:\n"); //prints 'enter key:' then starts new line 
+                scanf("%d", &key); // stores user input in integer 'key'
+    
+                rotationDecryption(inputText, key);
                 // 2. Decryption of a message encrypted with a rotation cipher given cipher text and rotation amount
                 break;
             case '3':
