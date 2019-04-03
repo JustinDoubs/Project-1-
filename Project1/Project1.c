4469818c87744c2c9153444bbca054fe1b190785
Justin Doubikin <justin.doubikin@uon.edu.au>
2019-04-03 10:35:56 +0000

    final for today

--- Project1/Project1.c
+++ Project1/Project1.c
@@ -1,19 +1,22 @@
 #include<stdio.h>
-
+void rotationDecryption(char *inputText, int key);
+void rotationEncryption(char *inputText, int key);
 int main() {
 
     int choice;
     
     //while (switchValid = 0) {
     
-        printf("Please enter 1 for 'Encryption of a message with a rotation cipher given the message text and rotation amount'\n"
+        printf("Please enter;\n"
+        "1 for 'Encryption of a message with a rotation cipher given the message text and rotation amount'\n"
         "2 for 'Decryption of a message encrypted with a rotation cipher given cipher text and rotation amount'\n"
         "3 for 'Encryption of a message with a substitution cipher given message text and alphabet substitution'\n"
         "4 for 'Decryption of a message encrypted with a substitution cipher given cipher text and substitutions'\n"
         "5 for 'Decryption of a message encrypted with a rotation cipher given cipher text only'\n"
         "6 for 'Decryption of a message encrypted with a substitution cipher given cipher text only'\n");
-    char inputText[100]; //  array with ample range for any text input
-    int key;        
+        char inputText[100] = {"Hello world."};
+        char cipherText[100] = {"Ifmmp xpsme."};//  
+        int key = 1;        
         
         
         scanf("%d",&choice);
@@ -21,26 +24,26 @@ int main() {
         switch (choice) {
             case '1':
                  // key* determines how far the message is translated 
-                printf("enter text to be encrypted:\n"); // takes user input for text to be encrypted
-                fgets(inputText, 100, stdin); // stores user user input in array 'inputText'  have used fgets in lieu of anything bettter as i dont know how to stroe white space in an array... 
+                //printf("enter text to be encrypted:\n"); // takes user input for text to be encrypted
+                //fgets(inputText, 100, stdin); // stores user user input in array 'inputText'  have used fgets in lieu of anything bettter as i dont know how to stroe white space in an array... 
                 
     
-                printf("enter key:\n"); //prints 'enter key:' then starts new line 
-                scanf("%d", &key); // stores user input in integer 'key'
+                //printf("enter key:\n"); //prints 'enter key:' then starts new line 
+               // scanf("%d", &key); // stores user input in integer 'key'
     
                 rotationEncryption(inputText, key);
                 
                 // 1. Encryption of a message with a rotation cipher given the message text and rotation amount
                 break;
             case '2': 
-                printf("enter text to be encrypted:\n"); // takes user input for text to be encrypted
-                fgets(inputText, 100, stdin); // stores user user input in array 'inputText'  have used fgets in lieu of anything bettter as i dont know how to stroe white space in an array... 
+                //printf("enter text to be encrypted:\n"); // takes user input for text to be encrypted
+                //fgets(inputText, 100, stdin); // stores user user input in array 'inputText'  have used fgets in lieu of anything bettter as i dont know how to stroe white space in an array... 
                 
     
-                printf("enter key:\n"); //prints 'enter key:' then starts new line 
-                scanf("%d", &key); // stores user input in integer 'key'
+                //printf("enter key:\n"); //prints 'enter key:' then starts new line 
+                //scanf("%d", &key); // stores user input in integer 'key'
     
-                rotationDecryption(inputText, key);
+                rotationDecryption(cipherText, key);
                 // 2. Decryption of a message encrypted with a rotation cipher given cipher text and rotation amount
                 break;
             case '3':
@@ -60,7 +63,71 @@ int main() {
         }
     
     }
+    
+    
+//ENcryption function
 
+void rotationEncryption(char *inputText, int key) {
+    
+    char x; //temp store of individual characters
+    int i = 0; // counter integer
+
+    
+    
+        for (i = 0; inputText[i] != '\0'; ++i) { //for loop that runs unless item of array is null not sure when this would be used this as it was used by others and well it works.
+          x = inputText[i]; //stores each individual character of 'inputText[]' in char variable 'x' for one rendition of the loop
+
+          if(x >= 'a' && x <= 'z') { //if the ascii value of a char from 'inputText' being stored in 'x' is greater than ascii value of 'a' and less than ascii value of 'z' then runs next line
+              x = x + key; // reassigns value of x as previous value of x + key value ||||| will change to x = x - 32; to change all lower case letters to uppercase 
+
+              if(x > 'z') //if the ascii value of 'x' is greater than the ascii value of 'z' then next line will run 
+                x = x - 'z' + 'a' - 1; //ascii value of x - 26 |||||||||||| should change this to mod 26 when i have re assigned alphabet chars to 0 to 26|||||||||||||
+
+          }else if(x >= 'A' && x <= 'Z') { 
+              x = x + key;
+
+              if (x > 'Z')
+                x = x -'Z' + 'A' - 1;
+          }
+         inputText[i] = x; //recompiles 'inputText[]' with individual chars from for loop which has encrypted them 
+
+     }
+
+    printf("Encrypted message: %s\n", inputText); //prints "Encrypted message: ************" then creates new line.
+}
+
+
+//decryption
+
+void rotationDecryption(char *cipherText, int key) {
+    
+    
+    char x1; //temp store of individual characters
+    int i = 0; // counter integer
+
+    
+    
+        for (i = 0; cipherText[i] != '\0'; ++i) { //for loop that runs unless item of array is null not sure when this would be used this as it was used by others and well it works.
+          x1 = cipherText[i]; //stores each individual character of 'inputText[]' in char variable 'x' for one rendition of the loop
+
+          if(x1 >= 'a' && x1 <= 'z') { //if the ascii value of a char from 'inputText' being stored in 'x' is greater than ascii value of 'a' and less than ascii value of 'z' then runs next line
+              x1 = x1 - key; // reassigns value of x as previous value of x + key value ||||| will change to x = x - 32; to change all lower case letters to uppercase 
+
+              if(x1 > 'z') //if the ascii value of 'x' is greater than the ascii value of 'z' then next line will run 
+                x1 = x1 - 'z' + 'a' - 1; //ascii value of x - 26 |||||||||||| should change this to mod 26 when i have re assigned alphabet chars to 0 to 26|||||||||||||
+
+          }else if(x1 >= 'A' && x1 <= 'Z') { 
+              x1 = x1 - key;
+
+              if (x1 > 'Z')
+                x1 = x1 -'Z' + 'A' - 1;
+          }
+         cipherText[i] = x1; //recompiles 'inputText[]' with individual chars from for loop which has encrypted them 
+
+     }
+
+    printf("Decrypted message: %s\n", cipherText); //prints "decrypted message: ************" then creates new line.
+}
 
 /*_________________________________________________________________________________________________________________________________________________________   
     
