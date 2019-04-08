4f5f834e759fff7169e4537e3c649f0a4fb5f020
Justin Doubikin <justin.doubikin@uon.edu.au>
2019-04-08 21:34:16 +0000

    Just revising the ocde before todays lecture and lab making few adjustments such as changing all a-z to A-Z

--- Project1/Project1.c
+++ Project1/Project1.c
@@ -15,14 +15,14 @@ int main() {
         "5 for 'Decryption of a message encrypted with a rotation cipher given cipher text only'\n"
         "6 for 'Decryption of a message encrypted with a substitution cipher given cipher text only'\n");
         char inputText[100] = {"Hello world."};
-        char cipherText[100] = {"Ifmmp xpsme."};//  
+        char cipherText[100] = {"Ifmmp xpsme."};
         int key = 1;        
         
         
         scanf("%d",&choice);
     
         switch (choice) {
-            case '1':
+            case 1:
                  // key* determines how far the message is translated 
                 //printf("enter text to be encrypted:\n"); // takes user input for text to be encrypted
                 //fgets(inputText, 100, stdin); // stores user user input in array 'inputText'  have used fgets in lieu of anything bettter as i dont know how to stroe white space in an array... 
@@ -35,7 +35,7 @@ int main() {
                 
                 // 1. Encryption of a message with a rotation cipher given the message text and rotation amount
                 break;
-            case '2': 
+            case 2: 
                 //printf("enter text to be encrypted:\n"); // takes user input for text to be encrypted
                 //fgets(inputText, 100, stdin); // stores user user input in array 'inputText'  have used fgets in lieu of anything bettter as i dont know how to stroe white space in an array... 
                 
@@ -73,23 +73,26 @@ void rotationEncryption(char *inputText, int key) {
     int i = 0; // counter integer
 
     
-    
+            
         for (i = 0; inputText[i] != '\0'; ++i) { //for loop that runs unless item of array is null not sure when this would be used this as it was used by others and well it works.
           x = inputText[i]; //stores each individual character of 'inputText[]' in char variable 'x' for one rendition of the loop
+            
+            if(x >= 'a' && x <= 'z') { //if the ascii value of a char from 'inputText' being stored in 'x' is greater than ascii value of 'a' and less than ascii value of 'z' then runs next line
+                x = x - 32;
+                //x = x + key; // reassigns value of x as previous value of x + key value ||||| will change to x = x - 32; to change all lower case letters to uppercase 
 
-          if(x >= 'a' && x <= 'z') { //if the ascii value of a char from 'inputText' being stored in 'x' is greater than ascii value of 'a' and less than ascii value of 'z' then runs next line
-              x = x + key; // reassigns value of x as previous value of x + key value ||||| will change to x = x - 32; to change all lower case letters to uppercase 
+              //if(x > 'z') //if the ascii value of 'x' is greater than the ascii value of 'z' then next line will run 
+              //  x = x - 'z' + 'a' - 1; //ascii value of x - 26 |||||||||||| should change this to mod 26 when i have re assigned alphabet chars to 0 to 26|||||||||||||
+            } 
+            if (x >= 'A' && x <= 'Z') {
+            x = x + key;
 
-              if(x > 'z') //if the ascii value of 'x' is greater than the ascii value of 'z' then next line will run 
-                x = x - 'z' + 'a' - 1; //ascii value of x - 26 |||||||||||| should change this to mod 26 when i have re assigned alphabet chars to 0 to 26|||||||||||||
-
-          }else if(x >= 'A' && x <= 'Z') { 
-              x = x + key;
-
-              if (x > 'Z')
-                x = x -'Z' + 'A' - 1;
+            if (x > 'Z')
+              x = x - 'Z' + 'A' - 1;
           }
-         inputText[i] = x; //recompiles 'inputText[]' with individual chars from for loop which has encrypted them 
+
+          inputText[i] = x; // recompiles 'inputText[]' with individual chars
+                            // from for loop which has encrypted them 
 
      }
 
